71cd886bfe2e5ec2c61ed4711345a54b3676494d
Laura <lotemi.b2020@gmail.com>
2024-02-18 10:36:01 +0100

    i made a few changes

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+function refreshweather(response) {
+    console.log(response.data);
+}
+function searchCity (city) {
+    let apiKey =  "bat64968o85ffb3bc959985a07a4afdd";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshweather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
